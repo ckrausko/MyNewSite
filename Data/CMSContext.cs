3d58e7ddb0b15e75ad8e5e22fa40de2c89d884ee
ckrausko <ckrausko@gmail.com>
2017-02-11 10:11:07 -0500

    added page model

---
+++ Data/CMSContext.cs
@@ -0,0 +1,14 @@
+﻿using MySite.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MySite.Data
+{
+    public class CMSContext : DbContext
+    {
+        public CMSContext(DbContextOptions<CMSContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Page> Page { get; set; }
+    }
+}
---
+++ Models/Page.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MySite.Models
+{
+    public class Page
+    {
+        public string ID { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
